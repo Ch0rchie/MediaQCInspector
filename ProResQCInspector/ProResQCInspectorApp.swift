ab7f864be06335320fc288e3922a877c1b43d4fc
David Gelb <david@thegelbs.com>
2026-07-27 12:37:28 -0700

    Initial Commit

---
+++ ProResQCInspector/ProResQCInspectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProResQCInspectorApp.swift
+//  ProResQCInspector
+//
+//  Created by gelbda53 on 7/27/26.
+//
+
+import SwiftUI
+
+@main
+struct ProResQCInspectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProResQCInspectorTests/ProResQCInspectorTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProResQCInspectorTests.swift
+//  ProResQCInspectorTests
+//
+//  Created by gelbda53 on 7/27/26.
+//
+
+import Testing
+@testable import ProResQCInspector
+
+struct ProResQCInspectorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
